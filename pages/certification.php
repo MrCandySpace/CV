be4e73c6800346331d8cd2fa88135e2a6ddd7020
MrCandy <100863652+MrCandySpace@users.noreply.github.com>
2022-07-09 18:12:43 +0200

    Update
    Changement 
    -> CSS
    ->mise en forme de la page de certification

--- pages/certification.php
+++ pages/certification.php
@@ -18,3 +18,89 @@
 
     </div>
 </section>
+
+
+<section id="certifSection2">
+    <div class="certif_linkedin">
+        <div class="certif_titre">
+            Certificat d’accomplissement LinkedIn Learning
+        </div>
+        <!-- ------------------ Copier collé ceci ----------------------- >
+
+        <div class="certif_description">
+            <img src="images/LinkedIn_logo_initials.png">
+            <div class="certif_texte">
+                <a href="lien">
+                    <p class="certif_texte1">Titre</p>
+                </a>
+            </div>
+        </div>
+        <div class="space"></div>
+
+        -->
+        <!---------------------------------------------------------------->
+
+        <!-- Ajout des nouveaux block ICI -->
+
+        <!---------------------------------------------------------------->
+        <div class="certif_description">
+            <img src="images/LinkedIn_logo_initials.png">
+            <div class="certif_texte">
+                <a href="https://www.linkedin.com/learning/certificates/ede47e0e456f3c251ae5b5777ce74cc401980f9b1e2beea634f172f0112dd57e?u=91027594">
+                    <p class="certif_texte1">Comment déjouer les ransomwares</p>
+                </a>
+            </div>
+        </div>
+        <div class="space"></div>
+        <!---------------------------------------------------------------->
+        <div class="certif_description">
+            <img src="images/LinkedIn_logo_initials.png">
+            <div class="certif_texte">
+                <a href="https://www.linkedin.com/learning/certificates/245ff1e2cd11c8ed74ff4f627f864970afde57ab30c64d8b496cf9d58ec3c137?u=91027594">
+                    <p class="certif_texte1">Découvrir Docker</p>
+                </a>
+            </div>
+        </div>
+        <div class="space"></div>
+        <!---------------------------------------------------------------->
+        <div class="certif_description">
+            <img src="images/LinkedIn_logo_initials.png">
+            <div class="certif_texte">
+                <a href="https://www.linkedin.com/learning/certificates/51a52370f78f7c46605679cdae45f7d28c0e4e6adbd48d507a8e1a52818c431b?u=91027594">
+                    <p class="certif_texte1">L'essentiel de Git</p>
+                </a>
+            </div>
+        </div>
+        <div class="space"></div>
+        <!---------------------------------------------------------------->
+        <div class="certif_description">
+            <img src="images/LinkedIn_logo_initials.png">
+            <div class="certif_texte">
+                <a href="https://www.linkedin.com/learning/certificates/eb47150479c941c08f0ae9e2a8fceddb6a78e339d49c7ea95ec0549cff9f83cd?u=91027594">
+                    <p class="certif_texte1">Les fondements de la sécurité informatique et de la cybersécurité</p>
+                </a>
+            </div>
+        </div>
+        <div class="space"></div>
+        <!---------------------------------------------------------------->
+        <div class="certif_description">
+            <img src="images/LinkedIn_logo_initials.png">
+            <div class="certif_texte">
+                <a href="https://www.linkedin.com/learning/certificates/de6acf618f76c9b44d6f802c24fc38ac42fa139cdedb1d134ddd65ca31ee60d1?u=91027594">
+                    <p class="certif_texte1">Les fondements du DevOps</p>
+                </a>
+            </div>
+        </div>
+        <div class="space"></div>
+        <!---------------------------------------------------------------->
+        <div class="certif_description">
+            <img src="images/LinkedIn_logo_initials.png">
+            <div class="certif_texte">
+                <a href="https://www.linkedin.com/learning/certificates/fe6c74e9d3f4dedcc3e4c04457c2999392014b2bb42e39b8c3867d5aa737609b?u=91027594">
+                    <p class="certif_texte1">Les fondements des réseaux</p>
+                </a>
+            </div>
+        </div>
+        <!---------------------------------------------------------------->
+    </div>
+</section>
--- pages/formation.php
+++ pages/formation.php
@@ -8,7 +8,7 @@
             <span>2022</span>
         </div>
         <div class="forma_description">
-            <p>Activités réalisés :
+            <p>Activités réalisées :
                 <li>Création d'un site sous prestashop</li>
                 <li>Passage de la certification Opquast</li>
             </p>
@@ -29,14 +29,14 @@
             <span>2019 - 2021</span>
         </div>
         <div class="forma_description">
-            <p>Activités réalisés :
+            <p>Activités réalisées :
                 <li>Création d'une entreprise et un parc informatique virtuel</li>
                 <li>Installation, gestion et maintenance de serveur virtualisé</li>
                 <li>Paramétrage et test de pare-feu</li>
-                <li>Sécurisation du réseau (zone démilitarisé)</li>
+                <li>Sécurisation du réseau (zone démilitarisée)</li>
             </p>
             <p>Compétences acquise :
-                <li>Meilleur organisation professionnel</li>
+                <li>Meilleure organisation professionnelle</li>
                 <li>Communication avec d'autres équipes</li>
                 <li>Recherche et travail en autonomie</li>
             </p>
@@ -55,12 +55,12 @@
         </div>
         <div class="forma_description">
             <p>Obtenue avec mention assez bien </p>
-            <p>Connaissance professionnel :
+            <p>Connaissance professionnelle :
                 <li>Installation, utilisation, maintenance de poste informatique
                     sous différant système d'exploitation</li>
             </p>
-            <p>Acquisition des connaissance de base en :
-                <li>l'électronique et des courants basse tentions</li>
+            <p>Acquisition des connaissances de base en :
+                <li>l'électronique et des courants basse tention</li>
                 <li>soudage et connectique de la fibre optique</li>
                 <li>domotique</li>
             </p>
