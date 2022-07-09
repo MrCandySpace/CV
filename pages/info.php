0287ce0876f7d20d9da4a7a82dfd9d08f28e3777
Candy Space <mrcandyspace05@gmail.com>
2022-07-09 20:45:19 +0200

    Add info
    ajout de la page info
    -> description
    -> langues
    -> causes
    
    Changement CSS
    -> ajout des balises pour info
    -> changement d'emplacement de balise

---
+++ pages/info.php
@@ -0,0 +1,84 @@
+<section id="infoSection">
+    <div class="info">
+        <div class="info_titre">
+            Description
+        </div>
+        <div class="info_description">
+            <div class="info_texte">
+                <p>Passionné d'informatique, je
+                    cherche à élargir mon domaine
+                    de compétence et mes
+                    connaissances techniques.
+                </p>
+                <p>En procédure d'intégration dans
+                    la formation d'Administrateur
+                    Système DevOps.
+                </p>
+                <p>Autodidacte en langage HTML,
+                    CSS, PHP, JS grâce à
+                    OpenClassroom et LinkedIn
+                    Learning.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="space"></div>
+
+    <div class="info">
+        <div class="info_titre">
+            Langues
+        </div>
+        <div class="info_description">
+            <div class="info_texte">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Langue</th>
+                            <th scope="col">Niveau</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        <tr>
+                            <td>Français</td>
+                            <td>Langue natale</td>
+                        </tr>
+                        <tr>
+                            <td>Anglais</td>
+                            <td>Compétence professionnelle</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="space"></div>
+
+    <div class="info">
+        <div class="info_titre">
+            Causes
+        </div>
+        <div class="info_description">
+            <div class="info_texte">
+                <li>Arts et culture</li>
+                <li>Formation</li>
+                <li>Environnement</li>
+                <li>Sciences et technologie</li>
+            </div>
+        </div>
+    </div>
+
+    <!-------- Ajout de block ----------------
+    <div class="info">
+        <div class="info_titre">
+
+        </div>
+        <div class="info_description">
+            <div class="info_texte">
+
+            </div>
+        </div>
+    </div>
+    ---------------------------------------->
+</section>
