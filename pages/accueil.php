74427aa98d487447049b3535317c17cca083e9fd
Candy Space <mrcandyspace05@gmail.com>
2022-07-10 19:53:39 +0200

    Add accueil & update CSS

---
+++ pages/accueil.php
@@ -0,0 +1,43 @@
+<!--
+ ________  ________  ________  _______           ________  ________  ________  ___  ___  _______   ___  ___          
+|\   __  \|\   __  \|\   ____\|\  ___ \         |\   __  \|\   ____\|\   ____\|\  \|\  \|\  ___ \ |\  \|\  \         
+\ \  \|\  \ \  \|\  \ \  \___|\ \   __/|        \ \  \|\  \ \  \___|\ \  \___|\ \  \\\  \ \   __/|\ \  \ \  \        
+ \ \   ____\ \   __  \ \  \  __\ \  \_|/__       \ \   __  \ \  \    \ \  \    \ \  \\\  \ \  \_|/_\ \  \ \  \       
+  \ \  \___|\ \  \ \  \ \  \|\  \ \  \_|\ \       \ \  \ \  \ \  \____\ \  \____\ \  \\\  \ \  \_|\ \ \  \ \  \____  
+   \ \__\    \ \__\ \__\ \_______\ \_______\       \ \__\ \__\ \_______\ \_______\ \_______\ \_______\ \__\ \_______\
+    \|__|     \|__|\|__|\|_______|\|_______|        \|__|\|__|\|_______|\|_______|\|_______|\|_______|\|__|\|_______|
+                                                                                                                     
+-->
+<section id="accueilSection">
+    <div class="accueil">
+        <div class="accueil_titre">
+
+        </div>
+        <div class="accueil_description">
+
+        </div>
+    </div>
+</section>
+
+<div class="space"></div>
+
+<section id="accueilSection2">
+    <div class="accueil_compte">
+        <div>
+            <a href="https://github.com/MrCandySpace">
+                <img src="images/github_PNG40.png">
+                <p>
+                    Mon Github
+                </p>
+            </a>
+        </div>
+        <div>
+            <a href="https://www.linkedin.com/in/nicolas-vinckier-678a47238">
+                <img src="images/LinkedIn_logo_initials.png">
+                <p>
+                    Mon LinkedIn
+                </p>
+            </a>
+        </div>
+    </div>
+</section>
